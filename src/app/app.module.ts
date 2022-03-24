1626325f7b902cad7bf1cc81ba31d31d0671d80d
Shoukry Mohammed <shoukrymm86@gmail.com>
2022-03-24 18:55:52 +0200

    preparing the environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { OrderComponent } from './components/order/order.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, RegisterComponent, LoginComponent, ProductsComponent, CartComponent, OrderComponent, DetailsComponent, ContactusComponent, AboutusComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
